d79d016bf93117dedb936e6d24cfb45b2f54c51d
kaungkhantcoder <kaungkhantcoder@gmail.com>
2026-03-30 23:53:01 +0630

    change 404 design

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -11,9 +11,9 @@ const NotFound = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted">
       <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">404</h1>
-        <p className="mb-4 text-xl text-muted-foreground">Oops! Page not found</p>
-        <a href="/" className="text-primary underline hover:text-primary/90">
+        <h1 className="mb-4 text-[10rem] text-black/50 font-bold">404</h1>
+        <p className="mb-4 text-xl text-[2rem] text-muted-foreground">Oops! Page not found</p>
+        <a href="/" className="text-primary text-[1.5rem] underline hover:text-primary/90">
           Return to Home
         </a>
       </div>
